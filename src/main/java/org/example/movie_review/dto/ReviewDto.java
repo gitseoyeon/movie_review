fc58c2ba0b670912669a57205b9f3b9ed586624a
gitseoyeon <tjdus553926@gmail.com>
2025-07-24 22:06:00 +0900

    fix: review valid 에러 수정

--- src/main/java/org/example/movie_review/dto/ReviewDto.java
+++ src/main/java/org/example/movie_review/dto/ReviewDto.java
@@ -1,8 +1,6 @@
 package org.example.movie_review.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +9,10 @@ import lombok.Setter;
 public class ReviewDto {
     private Long id;
     @NotNull(message = "평점을 입력해주세요.")
-    @Size(min = 1, max = 5, message = "평점은 최소 1점에서 최대 5점 사이로 입력 가능합니다.")
+    @Min(value = 1, message = "평점은 최소 1점에서 최대 5점 사이로 입력 가능합니다.")
+    @Max(value = 5, message = "평점은 최소 1점에서 최대 5점 사이로 입력 가능합니다.")
     private int rating;
+
     private String comment;
 
     @NotBlank(message = "평가자명을 입력해주세요")
